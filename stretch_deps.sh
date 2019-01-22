a4ba969733a14da580b35534abb0ebb067b919d7
Lawrence Nahum <lawrence@greenaddress.it>
2019-01-22 20:19:34 +0100

    upgrade to ndk r19 and latest debian stretch

--- stretch_deps.sh
+++ stretch_deps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-export NDK_VERSION=android-ndk-r18b
+export NDK_VERSION=android-ndk-r19
 export NDK_FILENAME=${NDK_VERSION}-linux-x86_64.zip
 
-sha256_file=4f61cbe4bbf6406aa5ef2ae871def78010eed6271af72de83f8bd0b07a9fd3fd
+sha256_file=c0a2425206191252197b97ea5fcc7eab9f693a576e69ef4773a9ed1690feed53
 
 apt-get -yqq update &> /dev/null
 apt-get -yqq upgrade &> /dev/null
@@ -16,14 +16,8 @@ echo "${sha256_file}  ${NDK_FILENAME}" | shasum -a 256 --check
 unzip -qq ${NDK_FILENAME} &> /dev/null
 rm ${NDK_FILENAME}
 
-/opt/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=arm-linux-androideabi-clang --install-dir=/opt/arm-linux-androideabi-clang
-/opt/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=x86-clang --install-dir=/opt/x86-clang
-/opt/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=x86_64-clang --install-dir=/opt/x86_64-clang
-/opt/${NDK_VERSION}/build/tools/make-standalone-toolchain.sh --use-llvm --stl=libc++ --platform=21 --toolchain=aarch64-linux-android-clang --install-dir=/opt/aarch64-linux-android-clang
-
-
 if [ -f /.dockerenv ]; then
     apt-get -yqq --purge autoremove unzip
     apt-get -yqq clean
-    rm -rf /var/lib/apt/lists/* /var/cache/* /tmp/* /usr/share/locale/* /usr/share/man /usr/share/doc /lib/xtables/libip6* /opt/${NDK_VERSION}
+    rm -rf /var/lib/apt/lists/* /var/cache/* /tmp/* /usr/share/locale/* /usr/share/man /usr/share/doc /lib/xtables/libip6*
 fi
